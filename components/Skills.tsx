219a359acdd2e9327a035a552edd958986d83bea
chip902 <68761150+chip902@users.noreply.github.com>
2024-10-21 14:11:17 -0400

    update skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,32 +1,52 @@
-'use client'
+"use client";
 
-import { Box, Heading, Wrap, WrapItem, Tag, Container } from '@chakra-ui/react'
+import { Box, Heading, Wrap, WrapItem, Tag, Container } from "@chakra-ui/react";
 
 const skills = [
-  "JavaScript", "TypeScript", "React", "Next.js", "Node.js", "Express",
-  "MongoDB", "PostgreSQL", "GraphQL", "REST APIs", "HTML", "CSS",
-  "Tailwind CSS", "Git", "Docker", "AWS", "Python", "Machine Learning"
-]
+	"JavaScript",
+	"TypeScript",
+	"React",
+	"Next.js",
+	"Node.js",
+	"Angular",
+	"Express",
+	"MongoDB",
+	"PostgreSQL",
+	"GraphQL",
+	"REST APIs",
+	"HTML",
+	"CSS",
+	"Chakra UI",
+	"Radix UI",
+	"Shadcn UI",
+	"Daisy UI",
+	"Tailwind CSS",
+	"Git",
+	"Docker",
+	"AWS",
+	"Python",
+	"AI/ML",
+];
 
 const Skills = () => {
-  return (
-    <Box as="section" id="skills" py={{ base: 20, md: 32 }}>
-      <Container maxW="4xl">
-        <Heading as="h2" size="2xl" textAlign="center" mb={16}>
-          My Skills
-        </Heading>
-        <Wrap justify="center" spacing={4}>
-          {skills.map((skill, index) => (
-            <WrapItem key={index}>
-              <Tag size="lg" variant="subtle" colorScheme="blue" py={2} px={4}>
-                {skill}
-              </Tag>
-            </WrapItem>
-          ))}
-        </Wrap>
-      </Container>
-    </Box>
-  )
-}
+	return (
+		<Box as="section" id="skills" py={{ base: 20, md: 32 }}>
+			<Container maxW="4xl">
+				<Heading as="h2" size="2xl" textAlign="center" mb={16}>
+					My Skills
+				</Heading>
+				<Wrap justify="center" spacing={4}>
+					{skills.map((skill, index) => (
+						<WrapItem key={index}>
+							<Tag size="lg" variant="subtle" colorScheme="blue" py={2} px={4}>
+								{skill}
+							</Tag>
+						</WrapItem>
+					))}
+				</Wrap>
+			</Container>
+		</Box>
+	);
+};
 
-export default Skills
+export default Skills;
